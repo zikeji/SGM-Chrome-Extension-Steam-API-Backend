d15320f123cda4747bbceb0eccd477982304361d
Caleb B <caleb@blankemeyer.us>
2018-06-16 21:10:11 -0400

    cache results (#3)

--- config.example.js
+++ config.example.js
@@ -1,7 +1,8 @@
 module.exports = {
   port: 6161,
-  logLevel: 'debug',
-  steamApiKey: '',
-  salt: '',
-  dailyLimit: 10000,
+  logLevel: 'debug', // what level of logging you want to see
+  steamApiKey: '', // API key for Steam
+  salt: '', // salt for hashing the browser footprint
+  dailyLimit: 10000, // how many requests a unique browser is allowed to make a day
+  cacheTimeout: 60, // timeout in seconds for cached results to be destroyed
 };
